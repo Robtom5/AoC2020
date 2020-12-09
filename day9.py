6232bc4cc4fe05be82353d328aff0044005d5256
Robert <robtom_5@mac.com>
2020-12-09 12:55:58 +0000

    tidying code

--- day9.py
+++ day9.py
@@ -1,25 +1,5 @@
 with open("./Day9Input.txt") as infile:
     contents = infile.readlines()
-# contents = """35
-# 20
-# 15
-# 25
-# 47
-# 40
-# 62
-# 55
-# 65
-# 95
-# 102
-# 117
-# 150
-# 182
-# 127
-# 219
-# 299
-# 277
-# 309
-# 576""".split("\n")
 
 PREAMBLE_LENGTH = 25
 XMAS_raw = [int(n) for n in contents]
@@ -58,7 +38,6 @@ def recursive_pair(preamble, remainingInput, starting_depth):
             deepest = pair_depth
         return deepest
     else:
-        # print(f"Deepest: {next_value} at {depth}")
         return (depth, next_value)
 
 
